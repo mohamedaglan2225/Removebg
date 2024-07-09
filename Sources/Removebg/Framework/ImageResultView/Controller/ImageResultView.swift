8e9454ca95c4588a7801aed4986e24db9495656c
Mohamed Aglan <mohamedaglan.dev@gmail.com>
2024-07-09 23:47:04 +0300

    fix result image crash

--- Sources/Removebg/Framework/ImageResultView/Controller/ImageResultView.swift
+++ Sources/Removebg/Framework/ImageResultView/Controller/ImageResultView.swift
@@ -26,8 +26,18 @@ class ImageResultView: UIViewController {
     //MARK: - LifeCycle Events -
     override func viewDidLoad() {
         super.viewDidLoad()
-//        imageView.image = imageResult
+        //        imageView.image = imageResult
     }
+    
+    
+    public init() {
+        super.init(nibName: "ImageResultView", bundle: Bundle.module)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
 
     //MARK: - Configure UI -
